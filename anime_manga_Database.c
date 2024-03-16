8b83bc86de98ab525a98be69a787769ed12787d1
UnderFox3 <162766854+UnderFox3@users.noreply.github.com>
2024-03-16 22:05:41 +0700

    Nathanael Ed. 1
    Menambah fungsi untuk membaca file.

--- anime_manga_Database.c
+++ anime_manga_Database.c
@@ -9,7 +9,7 @@ typedef struct animeDetails
 {
     char animeTitle[50];
     int episodes;
-    int animeScore;
+    float animeScore;
 
     struct animeDetails *next;
 } animeDetails;
@@ -17,14 +17,73 @@ typedef struct animeDetails
 typedef struct mangaDetails
 {
     char mangaTitle[50];
+	int volumes;
     int chapters;
-    int mangaScore;
+    float mangaScore;
 
     struct mangaDetails *next;
 } mangaDetails;
 
+void showAnime()
+{
+    animeDetails animeGet[20];
+    int listCount, itemCount = 0;
+
+    FILE *fileAnime = fopen("animeList.txt", "r");
+
+    while(!feof(fileAnime))
+	{
+		animeDetails copyAnime;
+
+		fscanf(fileAnime, "%[^#]#%d#%f\n", copyAnime.animeTitle, &copyAnime.episodes, &copyAnime.animeScore);
+		animeGet[itemCount] = copyAnime;
+		itemCount++;
+	}
+
+	printf("\n-------------------------------------------------\n");
+	printf("| No. | Title \t \t| Episodes | Rating |\n");
+	printf("-------------------------------------------------\n");
+
+	for(listCount = 0;listCount < itemCount;listCount++)
+	{
+		printf("| %d. | %s | %d | %.2f |\n", listCount + 1, animeGet[listCount].animeTitle, animeGet[listCount].episodes, animeGet[listCount].animeScore);
+	}
+
+	fclose(fileAnime);
+}
+
+void showManga()
+{
+	mangaDetails mangaGet[20];
+	int listCount, itemCount = 0;
+
+	FILE *fileManga = fopen("mangaList.txt", "r");
+
+	while(!feof(fileManga))
+	{
+		mangaDetails copyManga;
+
+		fscanf(fileManga, "%[^#]#%d#%d#%f\n", copyManga.mangaTitle, &copyManga.volumes, &copyManga.chapters, &copyManga.mangaScore);
+		mangaGet[itemCount] = copyManga;
+		itemCount++;
+	}
+
+	printf("\n-------------------------------------------------------\n");
+    printf("| No. | Title \t \t| Volumes | Chapters | Score |\n");
+    printf("-------------------------------------------------------\n");
+
+	for(listCount = 0;listCount < itemCount;listCount)
+	{
+		printf("| %d. | %s | %d | %d | %.2f |\n", listCount + 1, mangaGet[listCount].mangaTitle, mangaGet[listCount].volumes, mangaGet[listCount].chapters, mangaGet[listCount].mangaScore);
+	}
+
+	fclose(fileManga);
+}
+
 int main()
 {
+	animeDetails sendAnime;
+	mangaDetails sendManga;
     int choice;
 
     do
@@ -32,10 +91,22 @@ int main()
         printf("\n==========Menu==========\n");
         printf("1. Show anime list\n");
         printf("2. Show manga list\n");
+		printf("3. Insert favorite anime\n");
+		printf("4. Insert favorite manga\n");
         printf("(0) Exit\n\n");
 
         printf("Choice: ");
         scanf("%d", &choice);
+
+		switch(choice)
+		{
+			case 1: showAnime(); break;
+			case 2: showManga(); break;
+			case 3: printf("To be added later\n"); break;
+			case 4: printf("Also to be added later\n"); break;
+			case 0: printf("Exiting Program...\n"); break;
+			default: printf("Option not available\n");
+		}
     } while(choice != 0);
 
     return 0;
